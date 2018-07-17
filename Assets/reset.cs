962ec2c2041c0651c30c70fee32e5e2aae6245bd
PJBeans <phillip236.po@gmail.com>
2018-07-17 15:33:49 -0400

    Fix Game Over, mess with theming

---
+++ Assets/reset.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class reset : MonoBehaviour {
+	//This script is used to reset the current scene. It should be disabled prior to a public release on a store (E.g.: Google Play).
+	//The sprite also won't be used. Text is baked in; Buckets uses Text Meshes for buttons. "Reset.png" was reused from Stride Jump, and will likely be removed before release.
+	public int enabled = 0;
+
+	void OnMouseDown () {
+		if (enabled == 1) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		}
+		if (enabled == 0) {
+			Debug.Log ("\"Reset\" is disabled. Please set \"enabled\" to \"1.\"");
+}
+}
+}
